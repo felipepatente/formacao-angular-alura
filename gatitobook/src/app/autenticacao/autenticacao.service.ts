c291494d487a03d3bf9631cf80f48af2a1958d06
felipepatente <felipepatente@yahoo.com.br>
2022-03-21 21:15:41 -0300

    Utilizando o environment

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +18,7 @@ export class AutenticacaoService {
   ) { }
 
   autenticar(usuario?: string, senha?: string) : Observable<HttpResponse<any>>{
-      return this.httpClient.post('http://localhost:3000/user/login',
+      return this.httpClient.post(`${API}/user/login`,
       {
         userName: usuario,
         password: senha
