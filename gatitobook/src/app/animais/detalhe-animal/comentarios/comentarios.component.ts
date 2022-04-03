72af791d740843446723629631f12d82907f80c6
felipepatente <felipepatente@yahoo.com.br>
2022-04-03 11:27:06 -0300

    Criando o template e o componente do comentário

--- gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { switchMap, tap } from 'rxjs/operators';
+import { ComentariosService } from './comentarios.service';
+import { Comentarios } from './comentarios';
+import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comentarios',
@@ -6,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentariosComponent implements OnInit {
 
-  constructor() { }
+  @Input() id!: number;
+  comentarios$!: Observable<Comentarios>
+  comentarioForm!: FormGroup;
+
+  constructor(
+    private comentarioService: ComentariosService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.comentarios$ = this.comentarioService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario:['', Validators.maxLength(300)]
+    })
   }
 
+  gravar(): void{
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentarioService
+      .incluirComentario(this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentarioService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert('Comentário salvo');
+        })
+      );
+  }
 }
