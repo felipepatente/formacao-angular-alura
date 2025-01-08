700f92dcee1148e49433404002659bd8b3899bdf
felipepatente <felipepatente@yahoo.com.br>
2025-01-08 07:23:32 -0300

    Injeção de dependência

--- memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
     providedIn:'root'
@@ -6,6 +7,6 @@ import { Injectable } from "@angular/core";
 
 export class PensamentoService{
 
-    constructor(){}
+    constructor(private http: HttpClient){}
 
 }
