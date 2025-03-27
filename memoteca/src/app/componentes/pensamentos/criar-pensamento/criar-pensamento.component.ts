642196efa5e4ec286b34bbac24798116d673f47a
felipepatente <felipepatente@yahoo.com.br>
2025-03-27 08:05:04 -0300

    Habilitar/desabilitar botão

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -45,4 +45,12 @@ export class CriarPensamentoComponent implements OnInit {
     this.router.navigate(['/listarPensamento']);
   }
 
+  habilitarBotao(): string {
+
+    if(this.formulario.valid){
+      return 'botao';
+    }
+
+    return 'botao__desabilitado';
+  }
 }
