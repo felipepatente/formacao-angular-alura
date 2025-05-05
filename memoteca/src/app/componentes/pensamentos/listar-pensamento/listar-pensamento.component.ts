7a04bbec914a5633e1ccafc49d6525431f8319ef
felipepatente <felipepatente@yahoo.com.br>
2025-05-05 18:56:41 -0300

    Pesquisando pensamentos

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,20 +17,30 @@ export class ListarPensamentoComponent implements OnInit {
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamento) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamento) => {
       this.listaPensamentos = listaPensamento;
     });
   }
 
   carregarMaisPensamento(): void{
 
-    this.service.listar(++this.paginaAtual).subscribe(listaPensamento => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe(listaPensamento => {
       this.listaPensamentos.push(...listaPensamento);
 
       if(!listaPensamento.length)
         this.haMaisPensamentos = false;
 
     });
+  }
+
+  pesquisarPensamento(): void{
+    
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(listaPensamento => {
+      this.listaPensamentos = listaPensamento;
+    })
   }
 
 }
